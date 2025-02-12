ef79b26a504fa4ab909d768fc94ca6370ea82757
Ankit K Gautam <ankitanuj032@gmail.com>
2025-02-12 19:02:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
+const BASE_URL ="https://2024-03-06.currency-api.pages.dev/v1/currencies/";
 
   const dropdowns = document.querySelectorAll(".dropdown select");
   const btn = document.querySelector("form button");
   const fromCurr = document.querySelector(".from select");
   const toCurr = document.querySelector(".to select");
+  // console.log(toCurr);
+  const msg = document.querySelector(".msg");
 
   for (let select of dropdowns){
     for(currcode in countryList){
@@ -26,21 +26,32 @@ const BASE_URL =
   }
 
   const updateflag = (element)=>{
-    console.log(element.value);
     let currcode = element.value;
     let concode = countryList[currcode];
     let img = element.parentElement.querySelector("img");
     img.src =`https://flagsapi.com/${concode}/flat/64.png`;
   }
 
-  btn.addEventListener("click" , async (evt) => {
-    evt.preventDefault();
+  const updateExchangeRate = async () => {
     let amount = document.querySelector(".amount input");
-    let amountvalue = amount.value;
-    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+      amtVal = 1;
+      amount.value = "1";
+    }
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
     let response = await fetch(URL);
     let data = await response.json();
     let rate = data[toCurr.value.toLowerCase()];
-    console.log(rate);
-  });
+    let finalAmount = (data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()]*parseInt(amount.value));
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  };
   
+  btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+  });
+
+  window.addEventListener("load", () => {
+    updateExchangeRate();
+  });
